370dbe9ba7648e89e1ed49d48d17655dc08f56bb
Héctor Fabio Cadavid Rengifo <hector.cadavid@gmail.com>
2021-12-09 12:14:49 +0100

    extensions logging

--- Dummy-extension/src/main/java/rug/icdtools/dummy/extension/DummyMacro.java
+++ Dummy-extension/src/main/java/rug/icdtools/dummy/extension/DummyMacro.java
@@ -8,18 +8,11 @@ import org.asciidoctor.extension.InlineMacroProcessor;
 import java.util.HashMap;
 import java.util.Map;
 
-import java.util.logging.Logger;
-import java.util.logging.Level;
-import org.asciidoctor.jruby.log.internal.JULLogHandler;
-
-import org.asciidoctor.log.LogRecord;
-import org.asciidoctor.log.Severity;
-
+import rug.icdtools.logging.Logger;
 
 public class DummyMacro extends InlineMacroProcessor {
     
    
-    private final static JULLogHandler logger=new JULLogHandler();         
     
     public DummyMacro(String macroName) {
         super(macroName);        
@@ -30,13 +23,10 @@ public class DummyMacro extends InlineMacroProcessor {
     public Object process(ContentNode contentNode, String macroValue, Map<String, Object> attributes) {
 
         String twitterLink=null;
-        String twitterLinkText=null;
-                        
-        System.out.println(">>>>>>!!!!");
+        String twitterLinkText=null;                                
         
         if (macroValue == null || macroValue.isEmpty() || macroValue.equals("dummy")) {
-            LogRecord lr=new LogRecord(Severity.ERROR, "Dummy is not a valid word for dummy extension");            
-            logger.log(lr);            
+            Logger.log("TEST OF A CUSTOM LOGGER WITHIN DUMMY MACRO");
         } else {
             twitterLink = "https://www.twitter.com/" + macroValue;
             // Prepend twitterHandle with @ as text link:
