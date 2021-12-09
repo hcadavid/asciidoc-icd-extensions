c83f9691cd7b84c4e9ad2d9cc614a6ad1e5cb371
Héctor Fabio Cadavid Rengifo <hector.cadavid@gmail.com>
2021-12-09 12:04:59 +0100

    logging handler added

---
+++ asciidoctorj-extensions-logger/src/main/java/rug/icdtools/logging/ExtendedLoggerHandler.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rug.icdtools.logging;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.asciidoctor.log.LogHandler;
+import org.asciidoctor.log.LogRecord;
+
+/**
+ *
+ * @author hcadavid
+ */
+public class ExtendedLoggerHandler implements LogHandler {
+
+    
+    @Override
+    public void log(LogRecord logRecord) {
+        System.out.println(">>>>>>>>>>>>>>!!!!!!#### "+logRecord.getMessage()+","+logRecord.getSeverity());
+    }
+
+}
---
+++ asciidoctorj-extensions-logger/src/main/java/rug/icdtools/logging/Logger.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rug.icdtools.logging;
+
+/**
+ *
+ * @author hcadavid
+ */
+public class Logger {
+    
+    public static void log(String l){
+        System.out.println(">>>>>>>>>>>>>>!!!!!!#### "+l);
+    }
+    
+}
